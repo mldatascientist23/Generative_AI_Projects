8cec2693605e12b5d53ebc0ade9bcf6f7288f300
Hamesh Raj <hm.raisingani@gmail.com>
2024-08-10 09:17:17 +0500

    API key from user now.

--- gemini_chatbot/gemini_chatbot.py
+++ gemini_chatbot/gemini_chatbot.py
@@ -5,8 +5,9 @@ import streamlit as st
 
 # Configure the Google Generative AI API
 
-GOOGLE_API_KEY = "AIzaSyAZXviM7pYcIlnP0CAdasPlN4oA80OT-ds"
-genai.configure(api_key=GOOGLE_API_KEY)
+# GOOGLE_API_KEY = " "
+# genai.configure(api_key=GOOGLE_API_KEY)
+
 
 # Initialize the Model
 
@@ -26,17 +27,24 @@ def getResponseFromModel(user_input):
 st.markdown("##### Created By: **Engr. Hamesh Raj**")
 st.markdown("[Contact Here](https://www.linkedin.com/in/datascientisthameshraj)")
 
-st.title("Chatbot by using Gemini API")
+st.title(f"**Chatbot by using Gemini API**")
+
+# Sidebar for API key input
+st.sidebar.info("The model only gets Google Gemini API key.")
+api_key = st.sidebar.text_input("Enter your Google Gemini API Key Here:", type="password", placeholder="Paste your Gemini API Key")
 
 # Get a user input through a text input box
 
-user_input = st.text_input(f"**Enter your prompt:**", placeholder="write any query here.....")
+user_input = st.text_input(f"**Enter your prompt:**", placeholder="write any type of queries here.....")
 
 # Create a button that when clicked, it will generate the response
 
 if st.button("**Submit Query**"):
-    if user_input:
+    if api_key and user_input:
+        # configure the api key
+        genai.configure(api_key=api_key)
+
         output = getResponseFromModel(user_input)
         st.write(f"**Chatbot Answer:** {output}")
     else:
-        st.write("Please enter a prompt.")
+        st.write("Please enter both the API key and a prompt to generate a response.")
