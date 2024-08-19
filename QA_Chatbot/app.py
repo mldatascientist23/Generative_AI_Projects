c6fa1a1f3f9e95d8446485875faf0ee1c9d5db78
Hamesh Raj <151763477+mldatascientist23@users.noreply.github.com>
2024-08-19 08:45:48 +0500

    Update app.py

--- QA_Chatbot/app.py
+++ QA_Chatbot/app.py
@@ -3,12 +3,9 @@ from langchain_helper import create_vector_db, get_qa_chain
 
 # Streamlit Sidebar
 st.sidebar.title("FAQ Q&A Chatbot 🧑‍🏫")
-
-# File uploader for CSV file
 csv_file = st.sidebar.file_uploader("📄 Upload FAQ CSV File", type=["csv"])
 
 if csv_file:
-    # Button to create the vector database
     if st.sidebar.button("📥 Create Knowledgebase"):
         create_vector_db(csv_file)
         st.sidebar.success("Knowledgebase created successfully!")
@@ -17,15 +14,14 @@ if csv_file:
 st.title("FAQ Q&A Chatbot")
 st.markdown("Created by [**Engr. Hamesh Raj**](https://www.linkedin.com/in/datascientisthameshraj/)")
 
-# Input field for the question with a placeholder
 question = st.text_input("🤔 Ask a Question: ", placeholder="Type your question here... 💬")
 
-# Submit button
 if st.button("🔍 Query"):
     if question.strip() == "":
         st.warning("⚠️ Please enter a question before submitting!")
     else:
-        chain = get_qa_chain()
+        api_key = st.secrets["GOOGLE_API_KEY"]
+        chain = get_qa_chain(api_key)
         response = chain(question)
         st.header("📝 Answer")
         st.write(response["result"])
